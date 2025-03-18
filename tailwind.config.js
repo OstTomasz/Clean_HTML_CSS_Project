5147592c81531cd62359fdd655da99590932b7c0
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2025-03-18 19:23:21 +0100

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   content: ["./build/*.html"],
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+
   plugins: [],
 };
